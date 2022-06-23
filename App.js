0c2587e34ae88e79cadc18dd9414953615937a66
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-06-23 17:10:54 -0300

    Drawer Navigation

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navigation/NavigationStack';
 import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 import { StatusBar } from 'react-native';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar/>
+      {/* <StatusBar/> */}
 {/* <NavigationStack/> */}
-<NavigationTab/>
+{/* <NavigationTab/> */}
+<NavigationDrawer/>
     </NavigationContainer>
   );
 }
